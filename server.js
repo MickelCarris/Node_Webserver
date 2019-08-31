fb9a7d185c559ce81753a8417e21aeb9536639e9
MickelCarris <mickelcarris0007@gmail.com>
2019-08-31 20:32:25 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const user = require('./users');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.path} \n`;
+  fs.appendFile('server.log', log, err => {
+    if (err) throw err;
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Home Page',
+    body: 'Some text hbs'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    body: 'Some text hbs'
+  });
+});
+
+const staticFolder = path.join(__dirname, 'public');
+
+app.use(express.static(staticFolder));
+
+app.get('/api/user', (req, res) => {
+  res.json(user);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on Port:${PORT}`));
---
+++ users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    name: 'Yucy',
+    email: 'yu@yahoo.com'
+  },
+  {
+    name: 'Dave',
+    email: 'dv@yahoo.com'
+  },
+  {
+    name: 'Rone',
+    email: 'ro@yahoo.com'
+  }
+];
+
+module.exports = users;
